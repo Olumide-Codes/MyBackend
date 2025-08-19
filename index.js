e541bf5c93aad13f94841ddd85e1c2b4ef63746e
Olumide-Codes <bolorunduroolumide1@gmail.com>
2025-08-19 15:49:44 +0100

    added vercel.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const productRoutes = require('./routes/product.route');
 
 app.use('/myapi', productRoutes);
 app.use('/files', express.static('myfiles'));
+app.use('/files', express.static('myfiles'));
 app.listen(4200, (e)=>{
   console.log('the server is currently running on port 4200');
 });
