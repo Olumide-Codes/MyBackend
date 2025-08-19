d1e4f79195f19bfd08438ad8ffbcffe0f14f9dde
Olumide-Codes <bolorunduroolumide1@gmail.com>
2025-08-19 16:00:32 +0100

    added changes to index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const productRoutes = require('./routes/product.route');
 app.use('/myapi', productRoutes);
 app.use('/files', express.static('myfiles'));
 app.use('/files', express.static('myfiles'));
+
+app.get('/',(req, res)={
+  res.send('API running...')
+});
 app.listen(4200, (e)=>{
   console.log('the server is currently running on port 4200');
 });
